40d1eba2d4e2659175e9d6ce4be60b5f86d1b7de
cbs <canburak@msn.com>
2022-07-02 17:05:30 +0300

    ..

--- abihome/settings.py
+++ abihome/settings.py
@@ -136,18 +136,22 @@ USE_I18N = True
 USE_TZ = True
 
 
+STATIC_URL = os.environ.get('STATIC_URL', default='/static/')
+STATIC_ROOT = os.path.join(
+    BASE_DIR, os.environ.get('STATIC_ROOT', default='static'))
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATIC_URL = "/static/"
+# # Static files (CSS, JavaScript, Images)
+# # https://docs.djangoproject.com/en/1.9/howto/static-files/
+# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+# STATIC_URL = "/static/"
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+# # Extra places for collectstatic to find static files.
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 
 GRAPHENE = {
